3b9be67c6ddb433f27990662025430a4ddb4df03
Quốc Tuấn <147356491+vqt0210@users.noreply.github.com>
2024-09-17 13:32:39 +0700

    Delete routes.js

--- routes.js
+++
@@ -1,6 +0,0 @@
-const messageCtrl = require('./MessageCtrl');
-
-module.exports = function (app) {
-    app.post('/v1/messages', messageCtrl.sendMessage);
-    app.get('/v1/messages', messageCtrl.getMessage);
-};
